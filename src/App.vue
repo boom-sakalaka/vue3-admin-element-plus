2d3f29368e2358f49904009f32455117d19be00e
Gu <936285702@qq.com>
2021-11-14 10:46:09 +0800

    feat(other): feat(other): 添加element plus
    vue-cli-plugin-element-plus 方式添加element plus

--- src/App.vue
+++ src/App.vue
@@ -2,12 +2,12 @@
  * @Author: GZH
  * @Date: 2021-11-13 18:51:39
  * @LastEditors: GZH
- * @LastEditTime: 2021-11-13 23:09:53
+ * @LastEditTime: 2021-11-14 10:43:50
  * @FilePath: \vue3-admin\src\App.vue
  * @Description:
 -->
-<template>
-  <router-view />
-</template>
-
-<style lang="scss"></style>
+<template><router-view /></template>
+<script>
+export default { name: 'App' }
+</script>
+<style></style>
