5b1743a3016f84c8012173231279a813dfb0f995
Gu <936285702@qq.com>
2021-12-07 20:48:15 +0800

    feat(other): 添加占位menu
    添加占位 侧边栏 菜单

---
+++ src/layout/components/Sidebar/SidebarMenu.vue
@@ -0,0 +1,39 @@
+<!--
+ * @Author: GZH
+ * @Date: 2021-12-07 20:12:37
+ * @LastEditors: GZH
+ * @LastEditTime: 2021-12-07 20:20:07
+ * @FilePath: \vue3-admin\src\layout\components\Sidebar\SidebarMenu.vue
+ * @Description:
+-->
+<template>
+  <el-menu
+    default-active="2"
+    :unique-opened="true"
+    background-color="#545c64"
+    text-color="#fff"
+    active-text-color="#ffd04b"
+  >
+    <el-submenu index="1">
+      <template #title>
+        <i class="el-icon-location"> </i>
+        <span>导航一</span>
+      </template>
+      <el-menu-item index="1-1">选项一</el-menu-item>
+      <el-menu-item index="1-2">选项二</el-menu-item>
+    </el-submenu>
+    <el-menu-item index="2">
+      <i class="el-icon-location"></i>
+      <template #title>导航4</template>
+    </el-menu-item>
+  </el-menu>
+</template>
+
+<script>
+export default {
+  name: 'SidebarMenu'
+}
+</script>
+
+<script setup></script>
+<style lang="scss" scoped></style>
--- src/layout/components/Sidebar/index.vue
+++ src/layout/components/Sidebar/index.vue
@@ -2,15 +2,19 @@
  * @Author: GZH
  * @Date: 2021-11-15 20:33:47
  * @LastEditors: GZH
- * @LastEditTime: 2021-11-15 22:29:16
+ * @LastEditTime: 2021-12-07 20:45:49
  * @FilePath: \vue3-admin\src\layout\components\Sidebar\index.vue
  * @Description:
 -->
 <template>
-  <div>sidebar</div>
+  <div>
+    <h1>占位</h1>
+    <el-scrollbar> <SidebarMenu /></el-scrollbar>
+  </div>
 </template>
 
 <script setup>
 import {} from 'vue'
+import SidebarMenu from './SidebarMenu.vue'
 </script>
 <style lang="scss" scoped></style>
