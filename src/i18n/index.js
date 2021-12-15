d351860a26b276ed124fbb304393f2188b770550
Gu <936285702@qq.com>
2021-12-15 21:57:59 +0800

    feat(other): 添加自定义语言包
    添加语言切换组件

--- src/i18n/index.js
+++ src/i18n/index.js
@@ -2,21 +2,24 @@
  * @Author: GZH
  * @Date: 2021-12-15 20:09:30
  * @LastEditors: GZH
- * @LastEditTime: 2021-12-15 20:12:20
+ * @LastEditTime: 2021-12-15 21:56:56
  * @FilePath: \vue3-admin\src\i18n\index.js
  * @Description:
  */
 // 创建数据源
 import { createI18n } from 'vue-i18n'
+import zhLocale from './lang/zh'
+import enLocale from './lang/en'
+
 const messages = {
   en: {
     msg: {
-      test: 'hello world'
+      ...enLocale
     }
   },
   zh: {
     msg: {
-      test: '你好，世界'
+      ...zhLocale
     }
   }
 }
