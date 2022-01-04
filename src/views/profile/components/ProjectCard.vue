b6ab416d1e7070aab9fdc6541fd77edd162c30f1
Gu <936285702@qq.com>
2022-01-04 23:16:44 +0800

    feat(components): 添加头像自定义组件
    添加头像自定义组件 PanThumb

--- src/views/profile/components/ProjectCard.vue
+++ src/views/profile/components/ProjectCard.vue
@@ -2,15 +2,21 @@
  * @Author: GZH
  * @Date: 2022-01-04 22:06:09
  * @LastEditors: GZH
- * @LastEditTime: 2022-01-04 22:06:10
+ * @LastEditTime: 2022-01-04 23:15:37
  * @FilePath: \vue3-admin-element-plus\src\views\profile\components\ProjectCard.vue
  * @Description: 项目介绍
 -->
 <template>
-  <div>项目介绍</div>
+  <div>
+    <pan-thumb :image="$store.getters.userInfo.avatar" height="100px" width="100px">
+      <div>Hello</div>
+      {{ $store.getters.userInfo.title }}
+    </pan-thumb>
+  </div>
 </template>
 
 <script setup>
 import {} from 'vue'
+import PanThumb from '@/components/PanThumb'
 </script>
 <style lang="scss" scoped></style>
